c91a0e26cbd11a5c341dad413dd8b2e4007af50f
Vincent Do <144634658+vincentdont@users.noreply.github.com>
2025-11-10 19:10:01 -0700

    Hello everyone
    Hello everyone edit

--- main.cpp
+++ main.cpp
@@ -2,28 +2,8 @@
 
 int main()
 {
-    int x = 0, y = 0;
-
-    if(std::cin >> x)
-    {
-        int cnt = 1;
-        while(std::cin >> y)
-        {
-            if (y == x)
-            
-                ++cnt;
-            else{
-                std::cout << x << " occurs " << cnt << std::endl;
-
-                x = y;
-
-                cnt = 1;
-
-            }
-        }
-        std::cout << x << " occurs " << cnt << std::endl;
-    }
+    std::cout << "hello everyone" << std::endl;
 
     return 0;
 
-}
+}   
