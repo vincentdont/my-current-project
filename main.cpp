8e8d7919828673d4ad41b4b6754fa7a07ddb00f6
Vincent Do <144634658+vincentdont@users.noreply.github.com>
2025-11-10 19:07:43 -0700

    Hello Dev
    Hello dev edit on dev branch

--- main.cpp
+++ main.cpp
@@ -2,27 +2,7 @@
 
 int main()
 {
-    int x = 0, y = 0;
-
-    if(std::cin >> x)
-    {
-        int cnt = 1;
-        while(std::cin >> y)
-        {
-            if (y == x)
-            
-                ++cnt;
-            else{
-                std::cout << x << " occurs " << cnt << std::endl;
-
-                x = y;
-
-                cnt = 1;
-
-            }
-        }
-        std::cout << x << " occurs " << cnt << std::endl;
-    }
+    std::cout << "hello dev" << std::endl;
 
     return 0;
 
